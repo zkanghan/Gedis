b4d3b5888e6b479e6c41fb5f1fcb457125ebeb71
zhangkanghan <3224590571@qq.com>
2022-12-21 19:34:28 +0800

    ADD:flushAOF & Aof DoneHandler

--- aof.go
+++ aof.go
@@ -3,32 +3,44 @@ package main
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 )
 
 func rewriteAppendOnlyFileBackground() error {
-	//TODO: check if any coroutines are already being rewritten
-	//TODO: Parent-child coroutine communication
-	if server.aofRewriteState != -1 {
+	if server.aofRewriteChan != nil {
 		return errors.New("already rewriting")
 	}
+	defer func() {
+		if err := recover(); err != nil {
+			log.Printf("aof rewrite panic: %v \n", err)
+			server.aofRewriteChan <- false
+		}
+	}()
+
+	server.aofRewriteChan = make(chan bool, 1)
+	go func(tempfile string) {
+		if err := rewriteAppendOnlyFile(tempfile); err != nil {
+			server.aofRewriteChan <- false
+		} else {
+			server.aofRewriteChan <- true
+		}
+	}("temp-rewriteAof-bg.aof")
 	return nil
 }
 
 //This function also called when the save command is executed
+//it will overwrite the rewritten data to the 'filename' file
 func rewriteAppendOnlyFile(filename string) error {
 	now := GetTimeMs()
 	//create temp file
 	tempFile := "temp-rewriteAOF.aof"
 
-	fp, err := os.Create(tempFile)
-	if err != nil {
-		log.Printf("create temp file error when aof rewriting: %v \n", err)
-		return err
-	}
-	fp, err = os.OpenFile(tempFile, os.O_APPEND|os.O_WRONLY, 0666)
+	//if the file already exists, it is truncated
+	//if the file does not exist, it is created with mode 0666
+	fp, err := os.OpenFile(tempFile, os.O_APPEND|os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
 	if err != nil {
 		log.Printf("open file in rewriteAppendOnlyFile error: %v \n", err)
 		return err
@@ -106,7 +118,7 @@ wErr:
 func loadAppendOnlyFile(filename string) error {
 	//create a fake client
 	fakeClient := NewClient(0)
-	f, err := os.OpenFile(filename, os.O_RDONLY, 0)
+	f, err := os.OpenFile(filename, os.O_RDONLY|os.O_CREATE, 0666)
 	if err != nil {
 		log.Printf("open append only file for reading error: %v \n", err)
 		return err
@@ -165,6 +177,8 @@ func loadAppendOnlyFile(filename string) error {
 
 	// this point can only be reached when EOF is reached without errors.
 	_ = f.Close()
+	aofUpdateCurrentSize()
+	server.aofRewriteBaseSize = 0
 	return nil
 
 rErr:
@@ -176,9 +190,138 @@ fmtErr:
 	return errors.New("invalid aof file format")
 }
 
-//the main process calls this function when the child thread completes the AOF rewrite.
-func bgRewriteDoneHandler() {
-	//TODO
+//write the append only file buffer on disk.
+func flushAppendOnlyFile() {
+	if len(server.aofBuf) == 0 {
+		return
+	}
+	f, err := os.OpenFile(server.aofFileName, os.O_WRONLY|os.O_APPEND, 0)
+	if err != nil {
+		log.Printf("can't open append only file: %v \n", err)
+		return
+	}
+
+	n, err := f.Write([]byte(server.aofBuf))
+	if err != nil {
+		log.Printf("flush buffer to append only file error: %v \n", err)
+		if n > 0 {
+			server.aofCurrentSize += int64(n)
+			server.aofBuf = server.aofBuf[n:]
+		}
+		return
+	}
+
+	server.aofCurrentSize += int64(n)
+	server.aofBuf = ""
+}
+
+//the master goroutine calls this function when the child goroutine completes the AOF rewrite.
+func bgRewriteDoneHandler(exitFlag bool) {
+	if exitFlag == true {
+		f, err := os.OpenFile("temp-rewriteAof-bg.aof", os.O_APPEND|os.O_WRONLY, 0666)
+		if err != nil {
+			log.Printf("Unable to open the temporary AOF produced by the child: %v \n", err)
+			goto clearUp
+		}
+		if err = aofRewriteBufferWrite(f); err != nil {
+			log.Printf("Error trying to flush the parent diff to the rewritten AOF: %v \n", err)
+			_ = f.Close()
+			goto clearUp
+		}
+		//The only remaining thing to do is to rename the temporary file to the configured file
+		if err = os.Rename("temp-rewriteAof-bg.aof", server.aofFileName); err != nil {
+			goto clearUp
+		}
+
+		aofUpdateCurrentSize()
+
+		server.aofRewriteBaseSize = server.aofCurrentSize
+
+		server.aofBuf = ""
+	} else {
+		log.Printf("Background AOF rewrite terminated with error \n")
+	}
+
+clearUp:
+	server.aofRewriteBuf = make([]byte, 0)
+
+	_ = os.Remove("temp-rewriteAof-bg.aof")
+
+	server.aofRewriteChan = nil
+}
+
+//append the command to the AOF file or, if the AOF rewrite is in progress, to the AOF rewrite buffer.
+func feedAppendOnlyFile(cmd *GedisCommand, args []*GObj) error {
+	//Translate EXPIRE into PEXPIREAT
+	var buf string
+
+	if cmd.name == "expire" {
+		buf = catAppendOnlyExpireAtFile(cmd, args[1], args[2])
+	} else if cmd.name == "set" || cmd.name == "pexpireat" {
+		buf = catAppendOnlyGenericCommand(args)
+	} else {
+		return nil
+	}
+
+	server.aofBuf += buf
+
+	// If a background append only file rewriting is in progress we want to
+	// accumulate the differences between the child DB and the current one
+	// in a buffer, so that when the child process will do its work we
+	// can append the differences to the new append only file.
+	if server.aofRewriteChan != nil {
+		aofRewriteBufferAppend(buf)
+	}
+	return nil
+}
+
+//append data to the AOF rewrite buffer,
+func aofRewriteBufferAppend(s string) {
+	server.aofRewriteBuf = append(server.aofRewriteBuf, []byte(s)...)
+}
+
+//write the buffer into the file
+func aofRewriteBufferWrite(f *os.File) error {
+	buf := server.aofRewriteBuf
+	_, err := f.Write(buf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func aofUpdateCurrentSize() {
+	fi, err := os.Stat(server.aofFileName)
+	if err != nil {
+		log.Printf("Unable to obtain the AOF file length. stat: %v \n", err)
+		return
+	}
+	server.aofCurrentSize = fi.Size()
+}
+
+//create the string representation of an PEXPIREAT command
+func catAppendOnlyExpireAtFile(cmd *GedisCommand, key *GObj, seconds *GObj) string {
+	when := seconds.IntVal()
+	if cmd.name == "expire" {
+		when = GetTimeMs() + when*1000
+	}
+	// build 'pexpireat' command
+	args := []*GObj{
+		NewObject(STR, "pexpireat"),
+		key,
+		NewObject(STR, strconv.FormatInt(when, 10)),
+	}
+	return catAppendOnlyGenericCommand(args)
+}
+
+//restore the command to the protocol format
+func catAppendOnlyGenericCommand(args []*GObj) string {
+	genericCmd := fmt.Sprintf("*%d\r\n", len(args))
+	for i := 0; i < len(args); i++ {
+		str := args[i].StrVal()
+		genericCmd = genericCmd + fmt.Sprintf("$%d\r\n%s\r\n", len(str), str)
+	}
+	return genericCmd
 }
 
 func bytes2int(b []byte) int {
--- aof_test.go
+++ aof_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
@@ -17,9 +18,9 @@ func Test_AOF(t *testing.T) {
 		assert.Nil(t, err)
 	}
 
-	testFileName := "test_aof_rewrite.aof"
+	server.aofFileName = "test_aof_rewrite.aof"
 
-	err = rewriteAppendOnlyFile(testFileName)
+	err = rewriteAppendOnlyFile(server.aofFileName)
 	assert.Nil(t, err)
 
 	for i := 0; i < 8; i++ {
@@ -28,8 +29,14 @@ func Test_AOF(t *testing.T) {
 		assert.Nil(t, err)
 	}
 	//test load from aof
-	err = loadAppendOnlyFile(testFileName)
+	err = loadAppendOnlyFile(server.aofFileName)
 	assert.Nil(t, err)
+
+	var fi os.FileInfo
+	fi, err = os.Stat(server.aofFileName)
+	assert.Nil(t, err)
+
+	assert.Equal(t, fi.Size(), server.aofCurrentSize)
 
 	for i := 0; i < 8; i++ {
 		key := NewObject(STR, fmt.Sprintf("key%d", i))
@@ -38,4 +45,33 @@ func Test_AOF(t *testing.T) {
 		assert.NotNil(t, e)
 		assert.Equal(t, val.StrVal(), e.Val.StrVal())
 	}
+}
+
+func Test_catAppendOnlyExpireAtFile(t *testing.T) {
+	cmd := cmdTable[2]
+
+	s := catAppendOnlyExpireAtFile(&cmd, NewObject(STR, "key"), NewObject(STR, "5"))
+	assert.Equal(t, "*3\r\n$9\r\npexpireat\r\n$3\r\nkey\r\n$13", s[:31])
+}
+
+func Test_catAppendOnlyGenericCommand(t *testing.T) {
+	args := []*GObj{NewObject(STR, "set"), NewObject(STR, "key"), NewObject(STR, "val")}
+
+	s := catAppendOnlyGenericCommand(args)
+	assert.Equal(t, "*3\r\n$3\r\nset\r\n$3\r\nkey\r\n$3\r\nval\r\n", s)
+
+	args = []*GObj{NewObject(STR, "pexpireat"), NewObject(STR, "key"), NewObject(STR, "1234567890")}
+	s = catAppendOnlyGenericCommand(args)
+	assert.Equal(t, "*3\r\n$9\r\npexpireat\r\n$3\r\nkey\r\n$10\r\n1234567890\r\n", s)
+}
+
+func Test_autoRewriteAOF(t *testing.T) {
+	err := InitServer()
+	assert.Nil(t, err)
+
+	server.aofRewriteMinSize = 1
+	server.aofRewritePerc = 1
+	server.aofFileName = "test_appendOnly.aof"
+
+	ServerCron(server.aeloop, 0, 0)
 }
