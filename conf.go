8b4267e9f76844a65eebbcdd31f47600728fbc0c
zhangkanghan <3224590571@qq.com>
2022-12-22 10:23:45 +0800

    UPDATE:add aof config

--- conf.go
+++ conf.go
@@ -3,6 +3,11 @@ package main
 const (
 	PORT        int = 8888
 	MAX_CLIENTS int = 10000
+
+	AOF_AUTOSYNC_BYTES   int = 1024 * 1024 * 10
+	DEFULT_AOF_FILENAME      = "appendOnly.aof"
+	AOF_REWRITE_MIN_SIZE     = 1024 * 1024 * 32
+	AOF_REWRITE_PERC         = 80
 )
 
 // global variable
@@ -15,7 +20,11 @@ func InitServer() error {
 			data:   NewDict(DictType{HashFunc: HashStr, EqualFunc: EqualStr}),
 			expire: NewDict(DictType{HashFunc: HashStr, EqualFunc: EqualStr}),
 		},
-		clients: make(map[int]*GedisClient),
+		clients:           make(map[int]*GedisClient),
+		aofFileName:       DEFULT_AOF_FILENAME,
+		aofRewriteMinSize: AOF_REWRITE_MIN_SIZE,
+		aofRewritePerc:    AOF_REWRITE_PERC,
+		aofRewriteBuf:     make([]byte, 0),
 	}
 	var err error
 	server.aeloop, err = NewAeEventLoop()
