d4f429f27cb329e462daa4acb04c63e73b49ed22
zhangkanghan <zhangkanghan@wepie.com>
2023-07-12 16:41:31 +0800

    feat: init skipList

--- zset.go
+++ zset.go
@@ -1,5 +1,30 @@
 package main
 
+const MAX_LEVEL = 32
+
+type Element struct {
+	Member string
+	Score  float64
+}
+
+type Level struct {
+	forward *node
+	span    int64
+}
+
+type node struct {
+	Element
+	next  *node
+	level []*Level
+}
+
+type skipList struct {
+	header *node
+	tail   *node
+	length int64
+	level  int16
+}
+
 type ZSet struct {
 	//TODO: implement it
 }
